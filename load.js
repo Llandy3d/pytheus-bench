4259c65c533f08c3d8ce4b307fa05b068c03551a
Llandy Riveron Del Risco <llandy3d@gmail.com>
2023-05-06 00:27:49 +0200

    improve script

--- load.js
+++ load.js
@@ -2,11 +2,6 @@ import http from 'k6/http';
 
 import { sleep } from 'k6';
 
-// export const options = {
-//   vus: 10,
-//   duration: '2s',
-// };
-
 
 var vus = 20;
 var iterations = 100;
@@ -17,62 +12,42 @@ export const options = {
   scenarios: {
     pytheus_singleprocess: {
       executor: executor,
-      exec: 'pytheus_singleprocess',
+      // exec: 'pytheus_singleprocess',
       vus: vus,
       iterations: iterations,
       maxDuration: max_duration,
+      env: {PORT: '5000'},
     },
     pytheus_multiprocess: {
       executor: executor,
-      exec: 'pytheus_multiprocess',
+      // exec: 'pytheus_multiprocess',
       vus: vus,
       iterations: iterations,
       maxDuration: max_duration,
+      env: {PORT: '5001'},
     },
     old_singleprocess: {
       executor: executor,
-      exec: 'old_singleprocess',
+      // exec: 'old_singleprocess',
       vus: vus,
       iterations: iterations,
       maxDuration: max_duration,
+      env: {PORT: '5002'},
     },
     old_multiprocess: {
       executor: executor,
-      exec: 'old_multiprocess',
+      // exec: 'old_multiprocess',
       vus: vus,
       iterations: iterations,
       maxDuration: max_duration,
+      env: {PORT: '5003'},
     },
 
   },
 
 };
 
-// export default function () {
-//   http.get('http://localhost:5000');
-//   http.get('http://localhost:5001');
-//   http.get('http://localhost:5002');
-//   // http.get('http://localhost:5003');
-//   // http.get('http://localhost:5004');
-//   sleep(1);
-// }
-
-export function pytheus_singleprocess() {
-  http.get('http://localhost:5000');
-  sleep(1);
-}
-
-export function pytheus_multiprocess() {
-  http.get('http://localhost:5001');
-  sleep(1);
-}
-
-export function old_singleprocess() {
-  http.get('http://localhost:5002');
-  sleep(1);
-}
-
-export function old_multiprocess() {
-  http.get('http://localhost:5003');
+export default function () {
+  http.get(`http://localhost:${__ENV.PORT}`);
   sleep(1);
 }
